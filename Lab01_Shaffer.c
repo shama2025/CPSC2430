1980f924ff355d255a406167c3645bd1ba8d2a67
Marcus Shaffer <2003mashaffer@gmail.com>
2025-01-13 15:43:18 -0500

    *Create Makefile *Update Lab01_Shaffer.c

--- Lab01_Shaffer.c
+++ Lab01_Shaffer.c
@@ -7,18 +7,26 @@
 
 #include <stdio.h>
 #include <string.h>
+#define STR_LENGTH 35
 int main()
 {
 
-    char str[35]; // Array for string
+    char str[STR_LENGTH]; // Array for string
     int len = 0;  // Lenght of string
 
     // Get the string input
-    printf("Enter a string (Current length of string is %ld): ", sizeof(str));
+    printf("Enter a string (Current length of string is %d): ", STR_LENGTH -1 );
     fgets(str, sizeof(str), stdin);
 
     // Sets len to length of string
-    len = strlen(str);
+    len = strlen(str) -1;
+
+    // Replace new line with null
+    if(str[len] == '\n'){
+        str[len] = '\0';
+    }
+
+    printf("Your reversed string is: ");
 
     // Iterate in reverse order
     while (len != -1)
@@ -26,6 +34,9 @@ int main()
         printf("%c", str[len]);
         len--;
     }
+
+    // Print new line for readability
+    printf("\n");
 
     return 0;
 }
