117bb2a2d6aeea6f14f61937c2403dd7057379f6
Marcus Shaffer <2003mashaffer@gmail.com>
2025-01-13 13:00:34 -0500

    *Update .gitignore *Update Lab01_Shaffer

--- Lab01_Shaffer.c
+++ Lab01_Shaffer.c
@@ -2,18 +2,30 @@
  * @file Lab01_LastShaffer.c
  * @author Marcus Shaffer
  * @date 1/13/2025
- * @brief Take string input and print it backwards
+ * @brief Take string input and prints it backwards
  */
 
+#include <stdio.h>
+#include <string.h>
+int main()
+{
 
-int main(){
-
-    // Declare string array
+    char str[35]; // Array for string
+    int len = 0;  // Lenght of string
 
     // Get the string input
-    // fgets(pointer to array of characters, strlen(array), stdin)
+    printf("Enter a string (Current length of string is %ld): ", sizeof(str));
+    fgets(str, sizeof(str), stdin);
 
-    // Iterate in reverse order through the array and print each character
+    // Sets len to length of string
+    len = strlen(str);
+
+    // Iterate in reverse order
+    while (len != -1)
+    {
+        printf("%c", str[len]);
+        len--;
+    }
 
     return 0;
 }
