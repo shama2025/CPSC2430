a914bac40763bb555375ba32e058829abca6a35e
Marcus Shaffer <2003mashaffer@gmail.com>
2025-01-13 11:48:05 -0500

    Create Lab01_Shaffer.c

---
+++ Lab01_Shaffer.c
@@ -0,0 +1,20 @@
+/***
+ * @file Lab01_LastShaffer.c
+ * @author Marcus Shaffer
+ * @date 1/13/2025
+ * @brief Take string input and print it backwards
+ */
+
+
+int main(){
+
+    // Declare string array
+
+    // Get the string input
+    // fgets(pointer to array of characters, strlen(array), stdin)
+
+    // Iterate in reverse order through the array and print each character
+
+   
+    return 0;
+}
