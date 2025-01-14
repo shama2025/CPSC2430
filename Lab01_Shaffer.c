f9e04c556d0041b744afc6add18ffdebb8bb4198
Marcus Shaffer <2003mashaffer@gmail.com>
2025-01-14 13:23:05 -0500

    Update Lab01_Shaffer.c

--- Lab01_Shaffer.c
+++ Lab01_Shaffer.c
@@ -7,19 +7,19 @@
 
 #include <stdio.h>
 #include <string.h>
-#define STR_LENGTH 35
-int main()
+#define STR_LENGTH 40
+int main(void)
 {
 
     char str[STR_LENGTH]; // Array for string
-    int len = 0;  // Lenght of string
+    size_t len = 0;  // Lenght of string
 
     // Get the string input
-    printf("Enter a string (Current length of string is %d): ", STR_LENGTH -1 );
+    printf("Enter a string (Current length of accepted string is %d): ", STR_LENGTH -1 );
     fgets(str, sizeof(str), stdin);
 
-    // Sets len to length of string
-    len = strlen(str) -1;
+    // Sets len to length of string, C starts arrays at 0
+    len = strlen(str) - 1;
 
     // Replace new line with null
     if(str[len] == '\n'){
