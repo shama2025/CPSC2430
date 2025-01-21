bbdf6c34869917254860a563b809c296332ec582
shama2025 <2003mashaffer@gmail.com>
2025-01-21 14:35:47 -0500

    Create Lab02_Shaffer.c

---
+++ Lab02_Shaffer.c
@@ -0,0 +1,49 @@
+/***
+ * @file Lab02_LastShaffer.c
+ * @param argc number of parameters passed through command line
+ * @param argv array of parameters in command line
+ * @author Marcus Shaffer
+ * @date 1/21/2025
+ * @brief Takes filename as a parameter in cli and prints all opening and closing paranthesis
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define STR_SIZE 256
+int main(int argc, char *argv[])
+{
+    FILE *fp;              // File pointer
+    char string[STR_SIZE]; // String
+
+    if (argc != 2)
+    {
+
+        /**
+         * Handles the exception if there are not enough files
+         */
+
+        exit(1);
+    }
+
+    // Opens the file and gives it read access
+    fp = fopen(argv[1], "r");
+
+    while (!feof(fp)) { // Writes file to string line by line
+        fgets(string, STR_SIZE, fp);
+        for (int i = 0; i < strlen(string); i++) { // Iterates through string
+            if (string[i] == '{' || string[i] == '}' || string[i] == '(' || string[i] == ')') { // Checks if character meets conditin
+                
+                // Prints the character of the string
+                printf("%c", string[i]);
+            }
+        }
+    }
+
+    // Closes the file
+    fclose(fp);
+    
+    // Prints new line for readability
+    printf("\n");
+    return 0;
+}
