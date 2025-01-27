6cd0acb6199236f3db0ad9b51dff8ab45699622a
shama2025 <2003mashaffer@gmail.com>
2025-01-27 18:17:10 -0500

    *Update Labo03_Shaffer.c *Update lexar_text.c *Create makefile

---
+++ Lab03/Lab03_Shaffer.c
@@ -0,0 +1,11 @@
+/**
+ * @brief Read a file and return its following grammer
+ * @param argc Number of argumenst passed
+ * @param argv Array of arguments passed
+ * @return 
+ */
+#include <stdio.h>
+int main(int argc, char **argv){
+    printf("Hello World");
+    return 0;
+}
---
+++ Lab03/lexar_test.c
@@ -0,0 +1,8 @@
+int main(void){
+int i;
+int current = 1;
+for(i = 0; i < 100; i++){
+current = current * i;
+}
+return 0;
+}
